1160592a292eaf381246ca92fe79829e15318c1e
Liang Junheng <66333437@qq.com>
2025-06-22 06:55:59 +0800

    Update ClashBaseConfig.js

--- clash/ClashBaseConfig.js
+++ clash/ClashBaseConfig.js
@@ -9,7 +9,7 @@ const baseConfig = {
   'use-system-hosts': true,
   'find-process-mode': 'strict',
   'geodata-mode': true,
-  'geo-update-interval': 12,
+  'geo-update-interval': 15,
   'geo-auto-update': true,
   'geox-url': {
       geoip: `${githubMirror}https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.dat`,
@@ -254,7 +254,7 @@ const proxyGroups=[
 const ruleProviderCommon = {
   "type": "http",
   "format": "yaml",
-  "interval": 43200
+  "interval": 54000
 };
 // 规则集配置
 const ruleProviders = {
