26fe5450f525b2c18a0c83112b8c3b6eb1533996
Liang Junheng <66333437@qq.com>
2025-06-30 09:50:32 +0800

    Update ClashBaseConfig.js

--- clash/ClashBaseConfig.js
+++ clash/ClashBaseConfig.js
@@ -88,7 +88,6 @@ const dnsConfig = {
   // nameserver-policy没有命中时，走nameserver
   "nameserver": [...foreignDNS],
   "nameserver-policy": {
-    "rule-set:WorkingNet": system,
     "geosite:private": [...chinaDNS],
     "geosite:geolocation-!cn@cn": [...chinaDNS],
     "geosite:geolocation-cn@!cn": [...foreignDNS],
