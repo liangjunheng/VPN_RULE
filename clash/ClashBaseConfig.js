0bc56b2c6038bba47777d1be72a75b80e704ee04
Liang Junheng <66333437@qq.com>
2025-07-02 11:05:22 +0800

    Update ClashBaseConfig.js

--- clash/ClashBaseConfig.js
+++ clash/ClashBaseConfig.js
@@ -140,10 +140,10 @@ const tunConfig = {
 const groupBaseOption = {
   "interval": 300,
   "tolerance": 300,
-  "timeout": 2000,
+  "timeout": 3000,
   "url": "https://www.google.com/generate_204",
   "lazy": true,
-  "max-failed-times": 2,
+  "max-failed-times": 3,
   "hidden": false,
 };
 // 代理组筛选条件
